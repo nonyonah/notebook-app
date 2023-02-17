f65b1d60292537d1f2a4a5fb1b6a60e1f82e5539
nonyonah <nonyonah@gmail.com>
2023-02-17 19:17:47 +0100

    Initial Commit

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  // ignore: unused_element
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => VerifyEmailViewState();
+}
+
+class VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify Email'),
+      ),
+      body: Column(
+        children: [
+          const Text('Please verify your email'),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Send Email Verification'),
+          )
+        ],
+      ),
+    );
+  }
+}
