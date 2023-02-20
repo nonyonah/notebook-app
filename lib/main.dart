84e40d9f355a67d881f078665e73597f200a4cf8
nonyonah <nonyonah@gmail.com>
2023-02-20 22:31:16 +0100

    Improved login and verification

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ void main() {
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
-        //verifyEmailRoute: (context) => const VerifyEmailView(),
+        verifyEmailRoute: (context) => const VerifyEmailView(),
       },
     ),
   );
